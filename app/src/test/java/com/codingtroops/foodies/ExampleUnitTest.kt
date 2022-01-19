f8e22b794d298fd19dc811f0409db5e689f92670
JohnBuhanan <johnbuns@yahoo.com>
2022-01-19 14:10:49 -0700

    wip

--- app/src/test/java/com/codingtroops/foodies/ExampleUnitTest.kt
+++ app/src/test/java/com/codingtroops/foodies/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.codingtroops.foodies
 
+import com.codingtroops.foodies.model.data.FoodMenuRepository
+import com.codingtroops.foodies.ui.feature.categories.FoodCategoriesViewModel
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +12,13 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val dispatcher = TestCoroutineDispatcher()
+    private val repository = object : FoodMenuRepository by stub() {}
+    private val foodCategoriesViewModel = FoodCategoriesViewModel(
+        dispatcher = dispatcher,
+        repository = repository,
+    )
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
