33a4936348620933b5775579b38308b64a737da2
JohnBuhanan <johnbuns@yahoo.com>
2022-02-15 15:42:16 -0700

    retrofit

---
+++ common/retrofit/src/main/kotlin/com/johnbuhanan/common/retrofit/di/RetrofitModule.kt
@@ -0,0 +1,39 @@
+package com.johnbuhanan.common.retrofit.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitModule {
+    companion object {
+        const val API_URL = "https://www.themealdb.com/api/json/v1/1/"
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptorOkHttpClient(): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+    ): Retrofit {
+        return Retrofit
+            .Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(API_URL)
+            .build()
+    }
+}
