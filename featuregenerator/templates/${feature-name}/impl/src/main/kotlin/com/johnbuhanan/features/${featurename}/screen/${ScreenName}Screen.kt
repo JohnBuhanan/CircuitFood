e653c0d65e22e3f37bb77ba5ec4adcd7e7a3d174
JohnBuhanan <johnbuns@yahoo.com>
2022-02-15 15:16:46 -0700

    feature generator

--- featuregenerator/templates/${feature-name}/impl/src/main/kotlin/com/johnbuhanan/features/${featurename}/screen/${ScreenName}Screen.kt
+++ featuregenerator/templates/${feature-name}/impl/src/main/kotlin/com/johnbuhanan/features/${featurename}/screen/${ScreenName}Screen.kt
@@ -6,8 +6,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalContext
 import cafe.adriel.voyager.androidx.AndroidScreen
 import com.johnbuhanan.common.viewmodel.WithViewModel
-import com.johnbuhanan.features.${featurename}.viewmodel.${ScreenName}Effect.ShowToast
-import timber.log.Timber
+import com.johnbuhanan.features.${featurename}.view.${ScreeName}View
+import com.johnbuhanan.features.${featurename}.viewmodel.${ScreeName}Effect.ShowToast
+import com.johnbuhanan.features.${featurename}.viewmodel.${ScreeName}ViewModel
 
 class ${ScreenName}Screen : AndroidScreen() {
     @Composable
