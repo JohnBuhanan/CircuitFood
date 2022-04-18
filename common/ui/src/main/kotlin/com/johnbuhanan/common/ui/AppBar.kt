d69183df369eb4a7eff8009ab3ceccb1ef93a1e7
tinder-johnbuhanan <john.buhanan@gotinder.com>
2022-04-18 09:59:45 -0700

    first

--- common/ui/src/main/kotlin/com/johnbuhanan/common/ui/AppBar.kt
+++ common/ui/src/main/kotlin/com/johnbuhanan/common/ui/AppBar.kt
@@ -9,8 +9,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun AppBar() {
     TopAppBar(
