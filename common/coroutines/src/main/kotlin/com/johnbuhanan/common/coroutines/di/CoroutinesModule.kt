d35ce7cbe22b2b96db34686090f1bb24f95256f6
tinder-johnbuhanan <john.buhanan@gotinder.com>
2023-03-15 14:51:59 -0700

    CoroutinesModule.kt

--- common/coroutines/src/main/kotlin/com/johnbuhanan/common/coroutines/di/CoroutinesModule.kt
+++ common/coroutines/src/main/kotlin/com/johnbuhanan/common/coroutines/di/CoroutinesModule.kt
@@ -1,22 +1,20 @@
-//package com.johnbuhanan.common.coroutines.di
-//
-//import com.squareup.anvil.annotations.ContributesTo
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//
-//import dagger.hilt.components.SingletonComponent
-//import kotlinx.coroutines.CoroutineDispatcher
-//import kotlinx.coroutines.Dispatchers
-//import javax.inject.Singleton
-//
-//
-//@ContributesTo(AppScope::class)
-//@Module
-//class CoroutinesModule {
-//    @[Provides Singleton IODispatcher]
-//    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
-//
-//    @[Provides Singleton MainDispatcher]
-//    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
-//}
+package com.johnbuhanan.common.coroutines.di
+
+import com.johnbuhanan.common.di.AppScope
+import com.squareup.anvil.annotations.ContributesTo
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+
+@ContributesTo(AppScope::class)
+@Module
+class CoroutinesModule {
+    @[Provides Singleton IODispatcher]
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @[Provides Singleton MainDispatcher]
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
