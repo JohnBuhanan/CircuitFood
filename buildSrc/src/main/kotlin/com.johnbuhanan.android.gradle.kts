a162d612244a4cae76dac53e6de3c24fbec5b7d5
tinder-johnbuhanan <john.buhanan@gotinder.com>
2023-05-31 09:45:03 -0700

    AGP 8

--- buildSrc/src/main/kotlin/com.johnbuhanan.android.gradle.kts
+++ buildSrc/src/main/kotlin/com.johnbuhanan.android.gradle.kts
@@ -28,8 +28,8 @@ with(the<BaseExtension>()) {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildFeatures.compose = true
